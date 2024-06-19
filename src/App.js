b3aabe51722c7a7746eb5bcb646d342d596dea5c
Gaspar Scienza <gasparscienza@hotmail.com>
2024-06-19 12:31:02 -0300

    eleccion de imagenes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [linea1, setLinea1] = useState('');
+  const [linea2, setLinea2] = useState('');
+  const [imagen, setImagen] = useState('');
+
+  const onChangeLinea1 = function(valor){
+    setLinea1(valor.target.value);
+  }
+  const onChangeLinea2 = function(valor){
+    setLinea2(valor.target.value);
+  }
+
+  const onChangeImagen = function(img){
+    setImagen(img.target.value)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <select onChange={onChangeImagen}>
+        <option value="llamas">Casa en llamas</option>
+        <option value="futurama">Futurama</option>
+        <option value="Aliens">History</option>
+      </select> <br/>
+      <input onChange={onChangeLinea1} type='text' placeholder='Linea 1'/><br/>
+      <input onChange={onChangeLinea2} type='text' placeholder='Linea 2'/><br/>
+      <button>Exportar</button>
+
+      <div>
+        <span>{linea1}</span><br/>
+        <span>{linea2}</span><br/>
+        <img src={'/img/' + imagen +'.jpg'} />
+      </div>
     </div>
   );
 }
