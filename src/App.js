88c6e6463bdf68c9cb40b45be42fcdb285a88e69
GasparScienza <gasparscienza@hotmail.com>
2024-06-19 18:41:54 -0300

    Material ui estilos

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import { useState } from 'react';
 import './App.css';
+import FormControl from '@mui/material/FormControl';
+import Box from '@mui/material/Box';
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
 function App() {
 
@@ -20,19 +27,36 @@ function App() {
 
   return (
     <div className="App">
-      <select onChange={onChangeImagen}>
-        <option value="llamas">Casa en llamas</option>
-        <option value="futurama">Futurama</option>
-        <option value="Aliens">History</option>
-      </select> <br/>
-      <input onChange={onChangeLinea1} type='text' placeholder='Linea 1'/><br/>
-      <input onChange={onChangeLinea2} type='text' placeholder='Linea 2'/><br/>
-      <button>Exportar</button>
+      <div className='box-contenedor'>
+        <Box className="box" sx={{ minWidth: 120, maxWidth: 300}}>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Imagenes</InputLabel>
+            <Select 
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                onChange={onChangeImagen}  
+                label="Imagenes">
+              <MenuItem value="llamas.jpg">Casa en llamas</MenuItem>
+              <MenuItem value="futurama.jpg">Futurama</MenuItem>
+              <MenuItem value="Aliens.jpg">History</MenuItem>
+          </Select> <br/>
+          </FormControl>
+        </Box>
+      </div>
+      <div className='input'>
+        <TextField onChange={onChangeLinea1} margin="normal" id="outlined-basic" label="Linea 1" variant="outlined" /><br/>
+        <TextField onChange={onChangeLinea2} id="outlined-basic" label="Linea 2" variant="outlined" />
+      </div>
+
+      <Button variant="outlined">Exportar</Button>
 
       <div>
         <span>{linea1}</span><br/>
         <span>{linea2}</span><br/>
-        <img src={'/img/' + imagen +'.jpg'} />
+        {imagen != '' 
+          ? <img src={'/img/' + imagen} width={"500px"} />
+          : <></>
+        }
       </div>
     </div>
   );
